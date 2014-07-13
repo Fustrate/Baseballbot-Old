ad5b9c86b843612f35606a5b585a0855cf6782b7
Steven Hoffman <git@fustrate.com>
2014-07-12 20:59:45 -0700

    Add generic file writer method to CLI
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- lib/baseballbot/cli/base.rb
+++ lib/baseballbot/cli/base.rb
@@ -3,10 +3,14 @@ module Baseballbot
     module Base
       protected
 
-      def write_config(new_config)
-        File.open(Baseballbot.config_file, 'w') do |f|
-          f.write new_config.to_yaml
+      def write_file(path, contents)
+        File.open(path, 'w') do |f|
+          f.write contents
         end
+      end
+
+      def write_config(new_config)
+        write_file(Baseballbot.config_file, new_config.to_yaml)
       end
 
       def set_config_file
